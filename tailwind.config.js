414a33b9054ea82ec69e276366e8c730c3f22283
Yahya afadisse <yahyaafadisse92@gmail.com>
2024-11-05 15:36:18 +0100

    add tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*.html",       // For HTML files in the root
+    "./src/**/*.js",  // For JavaScript files in the src folder
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
